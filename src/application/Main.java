6d1cffaedf0b56a010538602810503aaaf4f815f
vitoref <vitorefinco@hotmail.com>
2020-10-23 15:09:06 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private static Scene mainScene;
+	
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
@@ -17,7 +19,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -26,6 +28,9 @@ public class Main extends Application {
 		}	
 	} 
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
